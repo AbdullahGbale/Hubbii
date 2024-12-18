82f45293b6955fcd4977a887ece94f85a4a95c76
iceking-fct <miracleamajama14@gmail.com>
2024-12-18 14:50:48 +0100

    urls.py files updated with the API routes

--- website/portfolio/views.py
+++ website/portfolio/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+# Create your views here;
+
+
+# List all posts or create a new one
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+
+# Retrieve, update, or delete a specific post
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
