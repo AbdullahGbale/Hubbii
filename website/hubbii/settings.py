f4ded77bb45ea484679235074adfc27532b3ace6
iceking-fct <miracleamajama14@gmail.com>
2024-12-18 16:08:00 +0100

    making progress

--- website/hubbii/settings.py
+++ website/hubbii/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio',  # New app
+     'rest_framework', # For API's
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
