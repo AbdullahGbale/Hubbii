4a48538d4e9dd263a7f158b11a572b2b5157c434
iceking-fct <miracleamajama14@gmail.com>
2024-12-20 16:35:26 +0100

    Update the API Views with Filtering and Searching

--- website/portfolio/urls.py
+++ website/portfolio/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import PostList, PostDetail
+from .views import ProjectListAPIView
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('projects/', ProjectListAPIView.as_view(), name='project-list'),
 ]
--- website/portfolio/views.py
+++ website/portfolio/views.py
@@ -1,3 +1,8 @@
+from .models import Project
+from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .serializers import ProjectSerializer
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Post
@@ -18,3 +23,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+
+class ProjectListAPIView(ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['status']  # Allow filtering by 'status'
+    search_fields = ['title', 'description']  # Allow searching by these fields
+    ordering_fields = ['created_at', 'title']  # Allow ordering by these fields
