1d86345afa2f256aef40b78705c7098f28722c53
iceking-fct <miracleamajama14@gmail.com>
2024-12-23 14:42:03 +0100

    add serializer for Portfolio model

--- website/portfolio/serializers.py
+++ website/portfolio/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from .models import Post
+from .models import Project
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'created_at']
 
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+
+class PortfolioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Portfolio
+        fields = '__all__'
