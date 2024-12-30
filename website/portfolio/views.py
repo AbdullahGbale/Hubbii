8e4c551fae9aa6e105a182b6a397ecefb04a6cdb
iceking-fct <miracleamajama14@gmail.com>
2024-12-30 15:43:43 +0100

    add register views

--- website/portfolio/views.py
+++ website/portfolio/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, redirect
 from .models import PortfolioSection, Contact, Experience, Skill
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 # Home View
 def home(request):
     return render(request, 'portfolio/home.html')
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
 
 # Portfolio Section Views
 def portfolio_section_list(request):
