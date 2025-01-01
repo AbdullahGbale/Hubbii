e17383f4c31a991ca64b69fd76ae49352a6b9b89
iceking-fct <miracleamajama14@gmail.com>
2025-01-01 18:29:59 +0100

    models.py updated

--- website/portfolio/models.py
+++ website/portfolio/models.py
@@ -62,3 +62,16 @@ class CustomSignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    file = models.FileField(upload_to='uploads/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
